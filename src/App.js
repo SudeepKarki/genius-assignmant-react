0fd26c2258c74c1359ae097a84758c59de035287
SudeepKarki <sudeep.karki7@gmail.com>
2023-12-31 18:20:08 +0545

    First Push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import Dashboard from "./components/Dashboard/Dashboard";
+import UserList from "./components/UserList/UserList";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUsers,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div className="App">
+        <div className="top-bar">
+          <h1>Front-end Developer Assignment</h1>
+        </div>
+        <div
+          className={`wrapper ${isOpen ? "sidebar-open" : "sidebar-close"} `}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <aside className="sidebar">
+            <button onClick={toggleSidebar} className="sidebar-toggle">
+              {isOpen ? (
+                <FontAwesomeIcon icon={faChevronLeft} />
+              ) : (
+                <FontAwesomeIcon icon={faChevronRight} />
+              )}
+            </button>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink activeclassname="active" to="/">
+                    <span>Dashboard</span> <FontAwesomeIcon icon={faHouse} />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeclassname="active" to="/subscriber-list">
+                    <span>Users</span> <FontAwesomeIcon icon={faUsers} />
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+          <main className="main">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="subscriber-list" element={<UserList />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
 
